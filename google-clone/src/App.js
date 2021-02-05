5580fdad39f1ad131a033319730487a54eba9eea
jungchangdae <einsvision@gmail.com>
2021-02-06 01:02:05 +0900

    Google clone coding..

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
 import './App.css';
 import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Google Clone🚀</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page.</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
 
       {/* Home (the one with the search on) */}
-      <Home />
+
       {/* SearchPage (The results page) */}
 
     </div>
--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -1,16 +1,31 @@
 import React from 'react'; //rfce를 쳐!
-import './Home.css'
+import './Home.css';
+import { Link } from "react-router-dom";
+import AppsIcon from '@material-ui/icons/Apps';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Search from '../Search';
+
 function Home() {
     return (
-        <div className='home'>
-            <h1>This is Homepage!</h1>
-            
+        <div className='home'>           
             <div className='home__header'>
-
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className='home__headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon/>
+                    <AccountCircleIcon/>
+                </div>
             </div>
 
             <div className='home__body'>
-                
+               <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' alt=''/> 
+            </div>
+            <div className="home__inputContainer">
+                <Search/>
             </div>
         </div>
     )
